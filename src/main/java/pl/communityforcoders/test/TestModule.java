d9eb89b71719a346633142a0368ea4023154b620
kacperduras <git@kacperduras.pl>
2018-01-04 16:41:32 +0100

    Test injections.

--- src/main/java/pl/communityforcoders/test/TestModule.java
+++ src/main/java/pl/communityforcoders/test/TestModule.java
@@ -1,7 +1,10 @@
 package pl.communityforcoders.test;
 
+import java.io.File;
 import net.dv8tion.jda.core.events.Event;
+import org.apache.commons.lang3.Validate;
 import pl.communityforcoders.nancy.Nancy;
+import pl.communityforcoders.nancy.module.annotation.Inject;
 import pl.communityforcoders.nancy.module.annotation.Listener;
 import pl.communityforcoders.nancy.module.annotation.Manifest;
 import pl.communityforcoders.nancy.module.annotation.OnDisable;
@@ -10,14 +13,27 @@ import pl.communityforcoders.nancy.module.annotation.OnEnable;
 @Manifest(name = "TestModule", author = "kacperduras", version = "1.0.0.0")
 public class TestModule {
 
+  @Inject
+  private Nancy nancy;
+
+  @Inject
+  private File directory;
+
+  @Inject
+  private Manifest manifest;
+
   @OnEnable
   public void onEnable(Nancy nancy) {
+    Validate.notNull(nancy);
+    Validate.notNull(this.nancy);
 
+    Validate.notNull(directory);
+    Validate.notNull(manifest);
   }
 
   @OnDisable
   public void onDisable(Nancy nancy) {
-
+    Validate.notNull(nancy);
   }
 
   @Listener
